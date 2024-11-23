84c64b84069067653c109801317bfea729e7957e
Theodor Laukart <theodor.laukart@icloud.com>
2024-11-23 16:05:25 +0100

    day 2

--- Python_100_days/day_02/task.py
+++ Python_100_days/day_02/task.py
@@ -1,1 +1,13 @@
- print("Hello"[4])
+print("Welcome to the tip calculator!")
+
+price = float(input("What was the total bill? $ "))
+
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+
+people = int(input("How many people to split the bill? "))
+
+newnumber = int(price)
+
+pay = round(((newnumber+(newnumber*(tip/100)))/people),3)
+
+print("Each person should pay:",price)
