a28d7b88c6a7feb2de7ed5cfc51e06d5b6c3ba84
Theodor Laukart <theodor.laukart@icloud.com>
2024-11-25 13:40:11 +0100

    day_3

---
+++ Python_100_days/day_03/task_1.py
@@ -0,0 +1,7 @@
+print("welcome to the rollercoaster!")
+height = int(input("what is your height in cm?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("sorry you have ot grow taller before you can ride")
---
+++ Python_100_days/day_03/task_2.py
@@ -0,0 +1,7 @@
+number1 =int(input("Write a number:"))
+number2 =int(input("Write a second number:"))
+
+if number1 % number2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
