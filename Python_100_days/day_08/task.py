fc470ea96ba34dc59042e6c561097e01c78ae667
Theodor Laukart <146285549+theoXlol@users.noreply.github.com>
2025-09-14 16:17:02 +0200

    .

--- Python_100_days/day_08/task.py
+++ Python_100_days/day_08/task.py
@@ -3,7 +3,7 @@ def greet():
     print("How are you")
     print("Good")
 
-greet()
+
 
 #Functions that allows for inputs
 
@@ -12,7 +12,7 @@ def greet_with_name(name):
     print(f"hello {name}")
     print(f"How do you do {name}?")
 
-greet_with_name("sigma")
+
 
 
 
@@ -20,4 +20,18 @@ def life_in_weeks(age):
     maths = 90 - age
     weeks = maths * 52
     print(f"You have {weeks} weeks left.")
-life_in_weeks(20)
+
+
+
+def caeser_cypher():
+    user_input = input("Type a message: \n").lower()
+    shift = int(input("Type a shift number: \n"))
+    cipher_text = ""
+    for letter in user_input:
+        position = ord(letter)
+        new_position = position + shift
+        new_letter = chr(new_position)
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+    
+caeser_cypher()
